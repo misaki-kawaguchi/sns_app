bbcffd176fc26925a18d7d4f4b9fb8d82de35e9a
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-06-26 15:55:14 +0900

    4.TimeLinePageのUI作成

--- lib/ui/account/account_page.dart
+++ lib/ui/account/account_page.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sns_app/models/account.dart';
 
-class AccountPage extends StatefulWidget {
+class AccountPage extends StatelessWidget {
   const AccountPage({Key? key}) : super(key: key);
-
-  @override
-  State<AccountPage> createState() => _AccountPageState();
-}
-
-class _AccountPageState extends State<AccountPage> {
-  Account myAccount = Account(
-    id: '1',
-    name: 'Flutterラボ',
-    imagePath:
-        'https://yt3.ggpht.com/ngVd2-zv5o3pGUCfiVdZXCHhnq_g1Lo1Y8DbrmB9O8G7DG0IWUQJgsacqsI_LRvZE8JTsbQIuQ=s900-c-k-c0x00ffffff-no-rj',
-    selfIntroduction: 'こんばんは',
-    userId: 'flutter_labo',
-    createdTime: DateTime.now(),
-    updatedTime: DateTime.now(),
-  );
 
   @override
   Widget build(BuildContext context) {
--- lib/ui/time_line/time_line_page.dart
+++ lib/ui/time_line/time_line_page.dart
@@ -1,14 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sns_app/models/account.dart';
+import 'package:sns_app/models/post.dart';
 
-class TimeLinePage extends StatelessWidget {
+class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
 
   @override
+  State<TimeLinePage> createState() => _TimeLinePageState();
+}
+
+class _TimeLinePageState extends State<TimeLinePage> {
+  Account myAccount = Account(
+    id: '1',
+    name: 'Flutterラボ',
+    imagePath:
+        'https://yt3.ggpht.com/ngVd2-zv5o3pGUCfiVdZXCHhnq_g1Lo1Y8DbrmB9O8G7DG0IWUQJgsacqsI_LRvZE8JTsbQIuQ=s900-c-k-c0x00ffffff-no-rj',
+    selfIntroduction: 'こんばんは',
+    userId: 'flutter_labo',
+    createdTime: DateTime.now(),
+    updatedTime: DateTime.now(),
+  );
+
+  List<Post> postList = [
+    Post(
+      id: '1',
+      content: '初めまして',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+    Post(
+      id: '2',
+      content: '初めまして2回目',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('タイムライン'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'タイムライン',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 1,
       ),
+      body: ListView.builder(
+          itemCount: postList.length,
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                border: index == 0
+                    ? const Border(
+                        top: BorderSide(color: Colors.grey, width: 0),
+                        bottom: BorderSide(color: Colors.grey, width: 0),
+                      )
+                    : const Border(
+                        bottom: BorderSide(color: Colors.grey, width: 0),
+                      ),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 22,
+                    foregroundImage: NetworkImage(myAccount.imagePath),
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  myAccount.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '@${myAccount.userId}',
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              DateFormat('M/d/yy')
+                                  .format(postList[index].createdTime!),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Text(postList[index].content),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
