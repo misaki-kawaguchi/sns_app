6ca5743e6f050ffd14f5aae99d9e8ab1f70c0726
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-06-26 16:45:18 +0900

    8.LoginPageのUI作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sns_app/ui/screen.dart';
+import 'package:sns_app/ui/start_up/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Screen(),
+      home: const LoginPage(),
     );
   }
 }
